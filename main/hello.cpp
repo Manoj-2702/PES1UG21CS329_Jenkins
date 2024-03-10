97aae7d839f26d92275aa7791add24fa3a682a09
Manoj Kumar H S <hsmanojkumar2003@gmail.com>
2024-03-10 10:28:19 +0530

    Adding Files

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World, PES1UG21CS329\n";
+  cout << "Hello, Jenkins, PES1UG21CS329\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
