bb7e3f3ce7c9f0da8ed360e13646918dd17ae504
Manoj Kumar H S <103581128+Manoj-2702@users.noreply.github.com>
2024-03-10 12:43:44 +0530

    new change to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World, PES1UG21CS329_Manoj\n";
   cout << "Hello, Jenkins, PES1UG21CS329_Manoj\n";
   cout << "I have successfully built and run\n";
+  cout << "This is a new Commit\n";
   return 0;
 }
